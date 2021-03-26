9e93b27a2c14149c2b1608e84deeb0539dde6150
etpig007 <50804178+etpig007@users.noreply.github.com>
2021-03-26 08:00:18 +0800

    Add files via upload

---
+++ 門診系統.py
@@ -0,0 +1,29 @@
+import pyautogui
+import time
+pyautogui.moveTo(25,25,duration=0.2)
+pyautogui.doubleClick()
+#輸入帳號
+pyautogui.moveTo(801,411,duration=0.2)
+pyautogui.click()
+pyautogui.typewrite('04053',interval=0.1)
+#輸入密碼
+pyautogui.moveTo(797,439,duration=0.2)
+pyautogui.click()
+pyautogui.typewrite('A44125',interval=0.1)
+pyautogui.press('enter')
+#按Enter
+pyautogui.moveTo(1018,411,duration=0.2)
+pyautogui.doubleClick()
+#門診系統
+pyautogui.moveTo(816,518,duration=0.2)
+pyautogui.doubleClick()
+pyautogui.press('enter')
+time.sleep(10)
+pyautogui.moveTo(200,200,duration=0.2)
+pyautogui.click()
+pyautogui.moveTo(434,421,duration=0.2)
+pyautogui.doubleClick()
+#離開提醒
+time.sleep(5)
+pyautogui.moveTo(629,648,duration=0.2)
+pyautogui.click()
